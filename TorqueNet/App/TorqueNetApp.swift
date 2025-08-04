ff74dd12b6bdaceafb1521b3a5680ec0eef0f021
brandyodhiambo <baodhiambo@co-opbank.co.ke>
2025-08-04 16:36:43 +0300

    configured firebase

--- TorqueNet/App/TorqueNetApp.swift
+++ TorqueNet/App/TorqueNetApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct TorqueNetApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ TorqueNet/Core/Firebase/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  TorqueNet
+//
+//  Created by MAC on 04/08/2025.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
